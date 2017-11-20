2c3db8cfd1173647325ad0a2ffcaa89e981b2b39
morgan <mog13@live.com>
2017-11-20 20:55:25 +0000

    added help response

---
+++ responses/help.js
@@ -0,0 +1,21 @@
+let responseHelper = require("../responseHelper");
+
+module.exports = function handleGetHelpRequest(intent, session, callback) {
+    // Set a flag to track that we're in the Help state.
+    if (session.attributes) {
+        session.attributes.userPromptedToContinue = true;
+    } else {
+        // In case user invokes and asks for help simultaneously.
+        session.attributes = {
+            userPromptedToContinue: true
+        };
+    }
+
+    let speechOutput = "To start a new game at any time, say, start new game. " +
+        "To repeat the last question, say, repeat. " +
+        "Would you like to keep playing?",
+        repromptText = "Would you like to keep playing?";
+    let shouldEndSession = false;
+    callback(session.attributes,
+        responseHelper.buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+};
