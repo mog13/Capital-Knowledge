6935a40408de276a6a90b7fab95cea9412f4bd42
morgan <mog13@live.com>
2017-11-20 21:07:22 +0000

    added a intent handler

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const countries = require("./questionData").countries;
-let responseHelper = require("./responseHelper");
-let gameController = require("./gameController");
 
+let responseHelper = require("./responseHelper");
 
 let welcomeResponse = require("./responses/welcome");
-
+let intentHandler = require("./responses/intentHandler");
 
 
 exports.handler = function(event, context) {
@@ -13,13 +11,12 @@ exports.handler = function(event, context) {
         if (event.request.type === "LaunchRequest") {
             welcomeResponse((sessionAttributes, speechletResponse) => {context.succeed(responseHelper.buildResponse(sessionAttributes, speechletResponse));});
         } else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
+            intentHandler(event.request,
                 event.session,
                 function callback(sessionAttributes, speechletResponse) {
                     context.succeed(responseHelper.buildResponse(sessionAttributes, speechletResponse));
                 });
         } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
             context.succeed();
         }
     } catch (e) {
